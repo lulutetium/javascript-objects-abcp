420d7a0804a73fd9385ef34b64155c67cedd38bd
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-14 20:40:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,12 @@
 var playlist = {
-  SPK: "Metal Dance";
+  SPK: "Metal Dance"
 }
 
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  
+}
